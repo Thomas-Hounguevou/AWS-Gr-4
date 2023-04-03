7d4e46b8303b87b10ff77c21c3815b9581dfc033
Hassan Jerrar <hassan.jerrar@ens.uvsq.fr>
2023-04-03 02:02:03 +0200

    recherche connection BDD

---
+++ js/Login.js
@@ -0,0 +1,11 @@
+import { auth,app } from "./ModuleLogin.js";
+
+function register() {
+    const email = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    console.log(email, password)
+}
+
+
+
