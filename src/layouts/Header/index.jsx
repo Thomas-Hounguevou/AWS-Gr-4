2c51fcc264b5635b512fa1bdaff65f9b3767759c
khier.talia <talia.khier-ext@korian.fr>
2023-04-17 00:39:31 +0100

    implement header

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './header.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <h1>Jeu du Pendu</h1>
+      <nav>
+        <Link to="/">Accueil</Link>
+        <Link to="/leaderboard">Leaderboard</Link>
+        <Link to="/regles">Règles et ajout de mots</Link>
+      </nav>
+      <button type="button">
+        <Link to="/signin">Login</Link>
+      </button>
+    </header>
+  );
 };
 
 export default Header;
