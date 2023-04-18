ac6b02b575c5e0fe169a00bcd8242535a1d63b05
khier.talia <talia.khier-ext@korian.fr>
2023-04-18 02:01:24 +0100

    remove counter

--- src/pages/Jeu/index.jsx
+++ src/pages/Jeu/index.jsx
@@ -7,40 +7,15 @@ import Word from '../../components/Word';
 import Popup from '../../components/PopUp';
 import NotificationComp from '../../components/Notification';
 
-const words = ['Hello World'];
-let selectedWord = words[Math.floor(Math.random() * words.length)];
+const words = ['helloworld'];
+let selectedWord = words[Math.floor(Math.random() * words.length)].trim();
 
 const Jeu = () => {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
   const [showNotification, setShowNotification] = useState(false);
-
-  // useEffect(() => {
-  //   var countDownDate = new Date().getTime() + 5 * 60 * 1000;
-
-  //   // Mettre à jour le compte à rebours toutes les secondes
-  //   var x = setInterval(function () {
-  //     // Récupérer la date et l'heure actuelles
-  //     var now = new Date().getTime();
-
-  //     // Calculer la distance entre maintenant et la date limite
-  //     var distance = countDownDate - now;
-
-  //     // Calculer les minutes et secondes restantes
-  //     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  //     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  //     // Afficher le compte à rebours dans l'élément avec l'id "countdown"
-  //     document.getElementById('countdown').innerHTML = minutes + 'm ' + seconds + 's ';
-
-  //     // Si la date limite est dépassée, afficher "EXPIRÉ"
-  //     if (distance < 0) {
-  //       clearInterval(x);
-  //       document.getElementById('countdown').innerHTML = 'EXPIRÉ';
-  //     }
-  //   }, 1000);
-  // }, []);
+  const [timer, setTimer] = useState('');
 
   useEffect(() => {
     const handleKeydown = event => {
@@ -81,6 +56,7 @@ const Jeu = () => {
   return (
     <>
       <Header />
+      <div className="timer">{timer ?? ''}</div>
       <div className="game-container">
         <Figure wrongLetters={wrongLetters} />
         <WrongLetters wrongLetters={wrongLetters} />
