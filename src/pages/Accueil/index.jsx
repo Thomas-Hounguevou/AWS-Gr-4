3616fc194204ec035e9168fb1fb3690ecbe5e09b
Abdelhadi Bouali <ha_bouali@esi.dz>
2023-04-19 00:58:01 +0100

    add accueil and regles page

--- src/pages/Accueil/index.jsx
+++ src/pages/Accueil/index.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
+import Pendu from '../../assets/gif/pendu.gif';
+import './acceuil.css';
+import { Link } from 'react-router-dom';
 function Accueil() {
-  return <div>Accueil</div>;
+  const [option, setOption] = React.useState('Facile');
+  return (
+    <div className="acceuil">
+      <img src={Pendu} alt="pendu" />
+      <div className="card">
+        <h1>Le Pendu</h1>
+        <div>
+          <h2>Jouer</h2>
+          <label className="label">Choix de difficulté</label>
+          <select
+            value={option}
+            className="select"
+            onChange={e => {
+              setOption(e.target.value);
+            }}
+          >
+            <option value="Facile">Facile</option>
+            <option value="Normal">Normal</option>
+            <option value="Difficile">Difficile</option>
+          </select>
+          <button>
+            <Link to="jeu">Jouer</Link>
+          </button>
+        </div>
+        <p>Ou</p>
+        <div>
+          <h2>Connecter pour Jouer enligne</h2>
+          <button>
+            <Link to="signin">Se Connecter</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Accueil;
--- src/pages/Regles/index.jsx
+++ src/pages/Regles/index.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import './regle.css';
+import { Link } from 'react-router-dom';
 
 const Regles = () => {
-  return <div>Regles</div>;
+  return (
+    <div className="regle_container">
+      <div className="card">
+        <div>
+          <h2>Règles du jeu</h2>
+          <p>
+            Le but du jeu est simple: il faut trouver le mot caché en devinant les lettres le
+            composant, vous disposez de 10 erreurs possible et de 5 minutes pour gagner la partie,
+            suite à la partie vous obtiendrez un score dépendant de votre nombre d'erreurs, de la
+            difficulté du mot, ainsi que le temps mis à trouver le mot. Si vous avez compris les
+            règles, cliquez sur le bouton "J'ai compris" ci-dessous pour jouer:
+          </p>
+          <button>
+            <Link to="/jeu">J'ai Compris</Link>
+          </button>
+        </div>
+
+        <div>
+          <h2>Entrez le mot que vous souhaiteriez ajouter</h2>
+          <input type="text" />
+          <button>Confirmer</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Regles;
