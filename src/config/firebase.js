a44338ee41797478f6239c8504e74e7c0114f2cd
Abdelhadi Bouali <ha_bouali@esi.dz>
2023-04-19 01:41:50 +0100

    add firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,8 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = firebase.initializeApp({
+const firebaseConfig = {
   apiKey: 'AIzaSyCw2s6XP0q-Bx1okAH5iqYwyr5V5N95gJ8',
   authDomain: 'aws-gr4.firebaseapp.com',
   databaseURL: 'https://aws-gr4-default-rtdb.europe-west1.firebasedatabase.app',
@@ -10,6 +11,9 @@ const firebaseConfig = firebase.initializeApp({
   messagingSenderId: '421758284621',
   appId: '1:421758284621:web:08b34ada45f92619197d04',
   measurementId: 'G-H3K18LKN1Q',
-});
+};
 
-export default firebaseConfig;
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
+export const db = getFirestore(app);
