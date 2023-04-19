d4c22ba764b6e218aa600be66a1f73b75f63972f
Ronan travail <23docherty.ronan@gmail.com>
2023-04-20 00:52:44 +0200

    tableau pour le leaderboard

--- js/Leaderboard.js
+++ js/Leaderboard.js
@@ -1,16 +1,10 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCw2s6XP0q-Bx1okAH5iqYwyr5V5N95gJ8",
-    authDomain: "aws-gr4.firebaseapp.com",
-    databaseURL: "https://aws-gr4.firebaseio.com/",
-    projectId: "aws-gr4",
-    storageBucket: "aws-gr4.appspot.com",
-    messagingSenderId: "421758284621",
-    appId: "1:421758284621:web:08b34ada45f92619197d04",
-    measurementId: "G-H3K18LKN1Q"
-};
-firebase.initializeApp(config);
+firebase.initializeApp({
+  apiKey: "AIzaSyCw2s6XP0q-Bx1okAH5iqYwyr5V5N95gJ8 ",
+  authDomain: "aws-gr4.firebaseapp.com",
+  projectId: "aws-gr4"
+});
 
-var ScoreJoueur = firebase.database().ref("ScoreJoueur");
+const ScoreJoueur = firebase.firestore().collection("ScoreJoueur");
 
 ScoreJoueur.onSnapshot(function(querySnapshot) {
 
@@ -28,9 +22,20 @@ ScoreJoueur.onSnapshot(function(querySnapshot) {
 
   var leaderboard = document.getElementById("leaderboard");
   leaderboard.innerHTML = "";
+  var headerRow = leaderboard.insertRow(0);
+  var nameHeader = headerRow.insertCell();
+  nameHeader.innerHTML = "<b>Nom</b>";
+  var scoreHeader = headerRow.insertCell();
+  scoreHeader.innerHTML = "<b>Score</b>";
+  var wordHeader = headerRow.insertCell();
+  wordHeader.innerHTML = "<b>Mot</b>";
   ScoresTrier.forEach(function(score) {
-    var li = document.createElement("li");
-    li.innerHTML = "Joueur: " + score.nom + " - Score: " + score.score + " - Mot: " + score.mot;
-    leaderboard.appendChild(li);
+    var row = leaderboard.insertRow();
+    var nameCell = row.insertCell();
+    nameCell.innerHTML = score.nom;
+    var scoreCell = row.insertCell();
+    scoreCell.innerHTML = score.score;
+    var wordCell = row.insertCell();
+    wordCell.innerHTML = score.mot;
   });
 });
