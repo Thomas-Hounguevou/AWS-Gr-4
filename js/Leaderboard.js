cc1b587e1e8815e3715c46ed2e00fdd62cf016f0
Ronan travail <23docherty.ronan@gmail.com>
2023-04-20 02:34:47 +0200

    modification sur le leaderboard

--- js/Leaderboard.js
+++ js/Leaderboard.js
@@ -1,41 +1,60 @@
 firebase.initializeApp({
-  apiKey: "AIzaSyCw2s6XP0q-Bx1okAH5iqYwyr5V5N95gJ8 ",
+  apiKey: "AIzaSyCw2s6XP0q-Bx1okAH5iqYwyr5V5N95gJ8",
   authDomain: "aws-gr4.firebaseapp.com",
   projectId: "aws-gr4"
 });
 
 const ScoreJoueur = firebase.firestore().collection("ScoreJoueur");
+const leaderboard = document.querySelector('#leaderboard');
+const montrerplus = document.querySelector('#montrerplus');
+let dernier = null;
 
-ScoreJoueur.onSnapshot(function(querySnapshot) {
+//Prend les valeurs dans notre base de données jusqu'à atteindre une limite et les trie par ordre décroissant
+function getScores(limit) {
+  let Bdd = ScoreJoueur.orderBy('Score', 'desc');
+  if (dernier) {
+    Bdd = Bdd.startAfter(dernier);
+  }
+  if (limit) {
+    Bdd = Bdd.limit(limit);
+  }
+  return Bdd.get();
+}
+//Affiche les donnes en html
+function Afficher(querySnapshot) {
+  querySnapshot.forEach(doc => {
+    const donnee = doc.donnee();
+    const Nom = donnee.Nom;
+    const Score = donnee.Score;
+    const Mot = donnee.Mot;
 
-  var scores = [];
-  querySnapshot.forEach(function(doc) {
-    var donnee = doc.data();
-    scores.push({
-      nom: donnee.Nom,
-      score: donnee.Score,
-      mot: donnee.Mot
-    });
+    const row = leaderboard.insertRow();
+    const Colplace = row.insertCell(0);
+    const Colnom = row.insertCell(1);
+    const Colscore = row.insertCell(2);
+    const Colmot = row.insertCell(3);
+
+    Colnom.innerText = Nom;
+    Colscore.innerText = Score;
+    Colmot.innerText = Mot;
+
+    if (dernier === null || Score < dernier) {
+      dernier = Score;
+      const place = leaderboard.rows.length - 1;
+    }
   });
+}
 
-  var ScoresTrier = scores.sort((a, b) => b.score - a.score);
+getScores(10).then(querySnapshot => {
+  Afficher(querySnapshot);
+});
 
-  var leaderboard = document.getElementById("leaderboard");
-  leaderboard.innerHTML = "";
-  var headerRow = leaderboard.insertRow(0);
-  var nameHeader = headerRow.insertCell();
-  nameHeader.innerHTML = "<b>Nom</b>";
-  var scoreHeader = headerRow.insertCell();
-  scoreHeader.innerHTML = "<b>Score</b>";
-  var wordHeader = headerRow.insertCell();
-  wordHeader.innerHTML = "<b>Mot</b>";
-  ScoresTrier.forEach(function(score) {
-    var row = leaderboard.insertRow();
-    var nameCell = row.insertCell();
-    nameCell.innerHTML = score.nom;
-    var scoreCell = row.insertCell();
-    scoreCell.innerHTML = score.score;
-    var wordCell = row.insertCell();
-    wordCell.innerHTML = score.mot;
+//si on clique montre 5 donnees de plus
+montrerplus.addEventListener('click', () => {
+  getScores(5).then(querySnapshot => {
+    Afficher(querySnapshot);
+    if (querySnapshot.size < 5) {
+      montrerplus.style.display = 'none';
+    }
   });
 });
